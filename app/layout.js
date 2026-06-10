a353fef731291980071b47b9784e209fa6a175ae
Mohamed Mahajne <m7md87.ot@gmail.com>
2026-06-11 01:03:45 +0300

    Add Google Analytics GA4 (G-4XDMFM5BLL)

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@ import './globals.css'
 import Providers from '@/components/Providers'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import Script from 'next/script'
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-4XDMFM5BLL'
 
 export const metadata = {
   title: 'KoraX - كأس العالم 2026 | توقعات ومباريات مونديال 2026',
@@ -62,6 +65,20 @@ export default function RootLayout({ children }) {
         </Providers>
         <Analytics />
         <SpeedInsights />
+
+        {/* Google Analytics GA4 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
       </body>
     </html>
   )
